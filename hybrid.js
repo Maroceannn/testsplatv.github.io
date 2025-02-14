5c6874a411b76af0f61bc106966b0a682fae349f
Ocean Zhou <2529724451@qq.com>
2025-02-14 16:18:10 +0800

    Update hybrid.js

--- hybrid.js
+++ hybrid.js
@@ -802,8 +802,8 @@ async function main() {
         } else if (splatData[0] == 75 && splatData[1] == 103) {
           // splatv file
           readChunks(new Response(splatData).body.getReader(), [{ size: 8, type: "magic" }], chunkHandler).then(() => {
-            currentCameraIndex = 0;
-            camera = cameras[currentCameraIndex];
+            //currentCameraIndex = 0;
+            //camera = cameras[currentCameraIndex];
             viewMatrix = getViewMatrix(camera);
           });
         } else {
